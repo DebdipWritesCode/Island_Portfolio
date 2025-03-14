e30d7dbb9da127a2059f5f33fdf89a912110a84b
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-03-14 11:05:34 +0530

    add floor and windows

---
+++ src/components/House/Windows.jsx
@@ -0,0 +1,19 @@
+import { useTexture } from "@react-three/drei"
+
+const Windows = (props) => {
+  const windowsTextures = useTexture('./Textures/WindowBaked.jpg')
+
+  return (
+    <>
+      <mesh
+        geometry={props.nodes.WindowsMerge.geometry}
+        position={[4.167, 2.987, 0.095]}
+        rotation={[0, -0.125, 0]}
+      >
+        <meshBasicMaterial map={windowsTextures} map-flipY={false} />
+      </mesh>
+    </>
+  )
+}
+
+export default Windows
--- src/components/Outdoor/House.jsx
+++ src/components/Outdoor/House.jsx
@@ -1,10 +1,12 @@
 import Bathroom from "../House/Bathroom"
 import Bedroom from "../House/Bedroom"
+import Floor from "../House/Floor"
 import Kitchen from "../House/Kitchen"
 import LivingRoom from "../House/LivingRoom"
 import Lunchroom from "../House/Lunchroom"
 import Roof from "../House/Roof"
 import Walls from "../House/Walls"
+import Windows from "../House/Windows"
 
 const House = (props) => {
   return (
@@ -16,6 +18,8 @@ const House = (props) => {
       <LivingRoom nodes={props.nodes} />
       <Lunchroom nodes={props.nodes} />
       <Bathroom nodes={props.nodes} />
+      <Floor nodes={props.nodes} />
+      <Windows nodes={props.nodes} />
     </>
   )
 }
